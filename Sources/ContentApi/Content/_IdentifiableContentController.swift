f0f75c337cf95a7a7683e0506bab1a2231b295c6
Tibor Bödecs <mail.tib@gmail.com>
2020-04-26 15:21:04 +0200

    identifiable content controller improvements

--- Sources/ContentApi/Content/_IdentifiableContentController.swift
+++ Sources/ContentApi/Content/_IdentifiableContentController.swift
@@ -8,31 +8,28 @@
 import Vapor
 import Fluent
 
-public protocol IdentifiableContentController: ContentController/*, Identifiable*/ {
+public protocol IdentifiableContentController: ContentController {
+
     var idParamKey: String { get }
-    func modelIdentifier(_ rawValue: String) throws -> Model.IDValue
+    var idPathComponent: PathComponent { get }
+    func find(_ req: Request) throws -> EventLoopFuture<Model>
 }
 
 public extension IdentifiableContentController {
 
     var idParamKey: String { "id" }
-    var idPathComponent: PathComponent { .init(stringLiteral: ":\(self.idParamKey)") }
-    
-    func find(_ req: Request) throws -> EventLoopFuture<Model> {
-        guard let rawValue = req.parameters.get(self.idParamKey) else {
-            throw Abort(.badRequest)
-        }
-        let id = try self.modelIdentifier(rawValue)
-        return Model.find(id, on: req.db).unwrap(or: Abort(.notFound))
-    }
+    var idPathComponent: PathComponent { .init(stringLiteral: ":" + self.idParamKey) }
 }
 
 public extension IdentifiableContentController where Model.IDValue == UUID {
 
-    func modelIdentifier(_ rawValue: String) throws -> Model.IDValue {
-        guard let id = UUID(uuidString: rawValue) else {
+    func find(_ req: Request) throws -> EventLoopFuture<Model> {
+        guard
+            let rawValue = req.parameters.get(self.idParamKey),
+            let id = UUID(uuidString: rawValue)
+        else {
             throw Abort(.badRequest)
         }
-        return id
+        return Model.find(id, on: req.db).unwrap(or: Abort(.notFound))
     }
 }
